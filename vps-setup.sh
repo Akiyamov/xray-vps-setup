3b4169e6dad3d98776441c771ddea34a44cb7c15
Akiyamov <AkiyamaSS@yandex.ru>
2024-12-29 15:29:44 +0500

    Edit script

--- vps-setup.sh
+++ vps-setup.sh
@@ -159,7 +159,6 @@ sshd_edit() {
   grep -r Port /etc/ssh -l | xargs -n 1 sed -i -e "/Port /c\Port $SSH_PORT"
   grep -r PasswordAuthentication /etc/ssh -l | xargs -n 1 sed -i -e "/PasswordAuthentication /c\PasswordAuthentication no"
   grep -r PermitRootLogin /etc/ssh -l | xargs -n 1 sed -i -e "/PermitRootLogin /c\PermitRootLogin no"
-  grep -r ChallengeResponseAuthentication /etc/ssh -l | xargs -n 1 sed -i -e "/PermitRootLogin /c\PermitRootLogin no"
   systemctl restart ssh
 }
 
